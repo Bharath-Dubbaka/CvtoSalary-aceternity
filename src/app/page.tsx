b10cdc23af45602bfcf77dceda77bc69ca30b881
Bharath Dubbaka <bharath.reactdev@gmail.com>
2024-04-07 21:53:26 +0530

    centered strcutured dhop items and added heading to shop as shop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,34 +53,8 @@ export default function Home() {
       <div className="max-w-5xl mx-auto px-8">
         <HoverEffect items={projects} />
       </div>
-
-      <div style={{display:"flex", flexWrap:"wrap", padding:"10%"}}>
-      <div style={{maxWidth:"20rem", padding: "1rem", justifyContent:"center"}}>
-      <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 bg-white dark:bg-zinc-900">
-        {/* <Image
-          src={`/images/stars.png`}
-          alt="jordans"
-          height="400"
-          width="400"
-          className="object-contain"
-        /> */}
-        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
-          Air Jordan 4 Retro Reimagined
-        </p>
- 
-        <p className="text-sm text-neutral-600 dark:text-neutral-400">
-          The Air Jordan 4 Retro Reimagined Bred will release on Saturday,
-          February 17, 2024. Your best opportunity to get these right now is by
-          entering raffles and waiting for the official releases.
-        </p>
-        <button className="rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800">
-          <span>Buy now </span>
-          <span className="bg-zinc-700 rounded-full text-[0.6rem] px-2 py-0 text-white">
-            $100
-          </span>
-        </button>
-        </BackgroundGradient>
-      </div>
+      <div style={{fontSize:"3rem", display:"flex", justifyContent:"center", padding:"2rem"}}>SHOP</div>
+      <div style={{display:"flex", justifyContent:"center", flexWrap:"wrap", padding:"5%, 10%, 5%, 10%"}}>
       <div style={{maxWidth:"20rem", padding: "1rem"}}>
       <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 bg-white dark:bg-zinc-900">
         {/* <Image
@@ -133,7 +107,33 @@ export default function Home() {
         </button>
         </BackgroundGradient>
       </div>
-      <div style={{maxWidth:"25rem", padding: "1rem"}}>
+      <div style={{maxWidth:"20rem", padding: "1rem"}}>
+      <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 bg-white dark:bg-zinc-900">
+        {/* <Image
+          src={`/images/stars.png`}
+          alt="jordans"
+          height="400"
+          width="400"
+          className="object-contain" 
+        /> */}
+        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+          Air Jordan 4 Retro Reimagined 
+        </p>
+ 
+        <p className="text-sm text-neutral-600 dark:text-neutral-400">
+          The Air Jordan 4 Retro Reimagined Bred will release on Saturday,
+          February 17, 2024. Your best opportunity to get these right now is by
+          entering raffles and waiting for the official releases.
+        </p>
+        <button className="rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800">
+          <span>Buy now </span>
+          <span className="bg-zinc-700 rounded-full text-[0.6rem] px-2 py-0 text-white">
+            $100
+          </span>
+        </button>
+        </BackgroundGradient>
+      </div>
+      <div style={{maxWidth:"20rem", padding: "1rem"}}>
       <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 bg-white dark:bg-zinc-900">
         {/* <Image
           src={`/images/stars.png`}
@@ -159,7 +159,7 @@ export default function Home() {
         </button>
         </BackgroundGradient>
       </div>
-      <div style={{maxWidth:"25rem", padding: "1rem"}}>
+      <div style={{maxWidth:"20rem", padding: "1rem"}}>
       <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 bg-white dark:bg-zinc-900">
         {/* <Image
           src={`/images/stars.png`}
