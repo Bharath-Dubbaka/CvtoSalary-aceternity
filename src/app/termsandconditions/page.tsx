e9d013bb00a0e40ffdab39e1e6e0a51c15b0e126
Bharath Dubbaka <bharath.reactdev@gmail.com>
2024-04-11 17:33:04 +0530

    added padding in tnc page

--- src/app/termsandconditions/page.tsx
+++ src/app/termsandconditions/page.tsx
@@ -25,7 +25,7 @@ export default function TandC (){
                 </div>
             </div>
             <div className="mt-4 font-normal text-base text-neutral-300  mx-auto" style={{paddingLeft:"5rem", paddingRight:"5rem", position:"relative", bottom:"8rem"}}>        
-                    <div>Terms & Conditions</div>
+                    <div style={{paddingTop:"2rem"}}>Terms & Conditions</div>
                     <div>USE OF DATA</div>
                     <div>The user hereby agrees and irrevocably authorizes us (Bookyourcv) to:</div>
 
@@ -33,12 +33,12 @@ export default function TandC (){
                     <div>b) use any data furnished by the member in order to float offers or send mails regarding specific services and such mails may not been proclaimed or deemed to be unsolicited communiqué.</div>
                     <div>c) Allow all data and information supplied by the member in using the service to remain at bookyourcv.com for the use of the company in accordance with service agreement with the member, notwithstanding the termination or suspension of the service to the member herein. If you share your linkedin credentials with us then you agree to update your profile online by our team, liking or commenting on our company page on your befalf.</div>
 
-                    <div>JOB GUARANTEE</div>
+                    <div style={{paddingTop:"2rem"}}>JOB GUARANTEE</div>
                     <div>We do not promise or take guarantee for any job or interview calls to the end user after taking our services. The services provided by us are our best effort and best in class and provided very economically. The selection in interview depends on many criteria like your current profile, experience, qualification etc. In today's scenario there is much competition in the market and this has to be understand by job seeker.</div>
                     <div>Payment card /Net banking details</div>
                     <div>You agree, understand and confirm that the card/Banking details provided by you for availing of services on www.bookyourcv.com will be correct and accurate and you shall not use the credit card /banking details which is not lawfully owned by you. We don't store any of your password or confidential information related to your credit/debit card or bank details. You further agree and undertake to provide the correct and valid credit card details to Payment Gateway Service provider. Further the said information will be not be utilized and shared by us with any of the third parties unless required by law, regulation or court order.</div>
 
-                    <div>TERM OF USE:</div>
+                    <div style={{paddingTop:"2rem"}}>TERM OF USE:</div>
                     <div>You hereby agree and promise not to use the services for any purpose that is restricted /unlawful or prohibited by these terms of use, By way of example, and not as a limitation, you agree not to use the Services:</div>
                     <div>1. To annoy, intimidate, abuse, impersonate, wrong comment on other user of www.bookyourcv.com</div>
                     <div>2. To transmit or post, or cause to be posted or transmitted, any content that is infringing, libellous, pornographic or otherwise violates any law or right of any third party, or corporate/firm or any other body of legal entity. Our two years free support will be only for new content modification arising out of new employment or new role. Already approved content cant be changed once Cv is closed or finally approved. You can come once in a quarter for these updations</div>
