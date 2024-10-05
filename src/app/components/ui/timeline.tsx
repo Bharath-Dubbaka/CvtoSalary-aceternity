62b770c3e9f2ac59e83a9709b172feca5ab3a793
Bharath Dubbaka <bharath.reactdev@gmail.com>
2024-10-05 17:33:38 +0530

    added new stpes compo with basic contents

--- src/app/components/ui/timeline.tsx
+++ src/app/components/ui/timeline.tsx
@@ -99,7 +99,7 @@ export function TimelineDemo() {
          content: (
             <div>
                <div className="flex">
-                  <div className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8 w-[50%] pl-0 p-2  flex items-center">
+                  <div className="text-neutral-800 dark:text-neutral-200  mb-8 w-[50%] pl-0 p-2  flex items-center">
                      At CVtoSalary.io, we make the process simple and secure.
                      Start by exploring our range of services tailored to meet
                      your career needs. Once you’ve selected the service that
@@ -127,7 +127,7 @@ export function TimelineDemo() {
                         className="rounded-lg object-cover h-20 md:h-60 lg:h-[15rem] w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
                      />
                   </div>
-                  <div className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8 w-[50%] pr-0 p-2 flex items-center">
+                  <div className="text-neutral-800 dark:text-neutral-200  mb-8 w-[50%] pr-0 p-2 flex items-center">
                      Your payment will be processed through a highly secure and
                      encrypted channel, ensuring your personal and financial
                      information is protected at every step. Rest assured, we
@@ -139,104 +139,129 @@ export function TimelineDemo() {
          ),
       },
       {
-         title: "Early 2023",
+         title: "Step 2",
          content: (
             <div>
-               <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-                  I usually run out of copy, but when I see content this big, I
-                  try to integrate lorem ipsum.
-               </p>
-               <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-                  Lorem ipsum is for people who are too lazy to write copy. But
-                  we are not. Here are some more example of beautiful designs I
-                  built.
-               </p>
-               <div className="grid grid-cols-2 gap-4">
-                  <Image
-                     src="https://assets.aceternity.com/pro/hero-sections.png"
-                     alt="hero template"
-                     width={500}
-                     height={500}
-                     className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-                  />
-                  <Image
-                     src="https://assets.aceternity.com/features-section.png"
-                     alt="feature template"
-                     width={500}
-                     height={500}
-                     className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-                  />
-                  <Image
-                     src="https://assets.aceternity.com/pro/bento-grids.png"
-                     alt="bento template"
-                     width={500}
-                     height={500}
-                     className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-                  />
-                  <Image
-                     src="https://assets.aceternity.com/cards.png"
-                     alt="cards template"
-                     width={500}
-                     height={500}
-                     className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-                  />
+               <div className="flex">
+                  <div className="text-neutral-800 dark:text-neutral-200 mb-8 w-[50%] pr-0 p-2 flex items-center">
+                     Within 24hrs, a representative from our team will get in
+                     touch with you to collect your information and learn about
+                     your preferences.
+                  </div>
+                  <div className=" w-[50%]">
+                     <Image
+                        src={sitPng}
+                        alt="startup template"
+                        width={500}
+                        height={500}
+                        className="rounded-lg object-cover h-20 md:h-60 lg:h-[15rem] w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+                     />
+                  </div>
+               </div>
+               <div className="flex">
+                  <div className=" w-[50%]">
+                     <Image
+                        src={applyPng}
+                        alt="startup template"
+                        width={500}
+                        height={500}
+                        className="rounded-lg object-cover h-20 md:h-60 lg:h-[15rem] w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+                     />
+                  </div>
+                  <div className="text-neutral-800 dark:text-neutral-200 mb-8 w-[50%] pr-0 p-2 flex items-center">
+                     This enables us to understand the basic overview which
+                     helps us to provide customized solution that best meets
+                     your requirements.
+                  </div>
                </div>
             </div>
          ),
       },
       {
-         title: "Changelog",
+         title: "Step 3",
          content: (
             <div>
-               <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-4">
-                  Deployed 5 new components on Aceternity today
+               <div className="flex">
+                  <div className="text-neutral-800 dark:text-neutral-200 mb-8 w-[50%] pr-0 p-2 flex items-center">
+                     Our knowledgeable writing staff looks over your profile and
+                     analyzes it in light of the most recent market and industry
+                     developments. Following a thorough examination and careful
+                     deliberation, they draft the initial version of your paper.
+                  </div>
+                  <div className=" w-[50%]">
+                     <Image
+                        src={sitPng}
+                        alt="startup template"
+                        width={500}
+                        height={500}
+                        className="rounded-lg object-cover h-20 md:h-60 lg:h-[15rem] w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+                     />
+                  </div>
+               </div>
+               <div>
+                  <div className="flex">
+                     <div className=" w-[50%]">
+                        <Image
+                           src={applyPng}
+                           alt="startup template"
+                           width={500}
+                           height={500}
+                           className="rounded-lg object-cover h-20 md:h-60 lg:h-[15rem] w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+                        />
+                     </div>
+                     <div className="text-neutral-800 dark:text-neutral-200 mb-8 w-[50%] pr-0 p-2 flex items-center">
+                        This draft is intended to draw attention to your
+                        abilities and match your qualifications with what
+                        companies are seeking in the cutthroat job market of
+                        today.
+                     </div>
+                  </div>
+               </div>
+            </div>
+         ),
+      },
+      {
+         title: "Step 4",
+         content: (
+            <div>
+               <p className="text-neutral-800 dark:text-neutral-200  mb-4">
+                  You provide feedback on the initial draft, and the group
+                  refines it until you're happy. After you give your approval,
+                  you receive the final version via email.
                </p>
                <div className="mb-8">
-                  <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
-                     ✅ Card grid component
+                  <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 ">
+                     ✅ 10 revisions
                   </div>
                   <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
-                     ✅ Startup template Aceternity
+                     ✅ 6 months support
                   </div>
                   <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
-                     ✅ Random file upload lol
+                     ✅ Random perk working with us 
                   </div>
                   <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
-                     ✅ Himesh Reshammiya Music CD
+                     ✅ Another Random perk
                   </div>
                   <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
-                     ✅ Salman Bhai Fan Club registrations open
+                     ✅ Something along the same lines
                   </div>
                </div>
                <div className="grid grid-cols-2 gap-4">
                   <Image
-                     src="https://assets.aceternity.com/pro/hero-sections.png"
+                     src={sitPng}
                      alt="hero template"
                      width={500}
                      height={500}
                      className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
                   />
                   <Image
-                     src="https://assets.aceternity.com/features-section.png"
+                     src={applyPng}
                      alt="feature template"
                      width={500}
                      height={500}
                      className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
                   />
-                  <Image
-                     src="https://assets.aceternity.com/pro/bento-grids.png"
-                     alt="bento template"
-                     width={500}
-                     height={500}
-                     className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-                  />
-                  <Image
-                     src="https://assets.aceternity.com/cards.png"
-                     alt="cards template"
-                     width={500}
-                     height={500}
-                     className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-                  />
+                  
                </div>
             </div>
          ),
