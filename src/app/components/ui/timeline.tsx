2f50c7d09859c5cb7b7c9bf94b4babe29e355248
Bharath Dubbaka <bharath.reactdev@gmail.com>
2024-10-22 01:15:48 +0530

    some ui/ux style changes

--- src/app/components/ui/timeline.tsx
+++ src/app/components/ui/timeline.tsx
@@ -48,9 +48,9 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
          ref={containerRef}
       >
          <div className="max-w-7xl mx-auto py-10 px-4 md:px-8 lg:px-10">
-            <div className="text-3xl md:text-5xl mb-4 text-black dark:text-white max-w-4xl">
+            <h2 className="text-lg md:text-4xl mb-4 text-black dark:text-white max-w-4xl">
                HOW WE WORK:
-            </div>
+            </h2>
             <p className="text-neutral-700 dark:text-neutral-300 text-sm md:text-base max-w-sm">
                I&apos;ve been working on Aceternity for the past 2 years.
                Here&apos;s a timeline of my journey.
